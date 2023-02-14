0c3592523c9af94481d935d3cfd1ff76a73cd0bb
hyoin08 <benjaminkim0228@gmail.com>
2023-02-14 21:02:07 +0900

    calculator description

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
-console.log(5 + 2);
-console.log(5 * 2);
-console.log(5 / 2);
+const calculator = {
+  plus: function (a, b) {
+    return a + b;
+  },
+  minus: function (a, b) {
+    return a - b;
+  },
+  times: function (a, b) {
+    return a * b;
+  },
+  divide: function (a, b) {
+    return a / b;
+  },
+  power: function (a, b) {
+    return a ** b;
+  },
+};
+
+const plusResult = calculator.plus(2, 3);
+const minusResult = calculator.minus(plusResult, 10);
+const timesResult = calculator.times(23, minusResult);
+const divideResult = calculator.divide(50, timesResult);
+const powerResult = calculator.power(divideResult, 2);
+console.log(plusResult);
+console.log(minusResult);
+console.log(timesResult);
+console.log(divideResult);
+console.log(powerResult);
