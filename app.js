9ca533075bbe1e9243448a39501c51bddd6d5b22
hyoin08 <benjaminkim0228@gmail.com>
2023-04-14 15:49:01 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,5 @@
-const calculator = {
-  plus: function (a, b) {
-    return a + b;
-  },
-  minus: function (a, b) {
-    return a - b;
-  },
-  times: function (a, b) {
-    return a * b;
-  },
-  divide: function (a, b) {
-    return a / b;
-  },
-  power: function (a, b) {
-    return a ** b;
-  },
-};
+const age = parseInt(prompt("How old are you?"));
 
-const plusResult = calculator.plus(2, 3);
-const minusResult = calculator.minus(plusResult, 10);
-const timesResult = calculator.times(23, minusResult);
-const divideResult = calculator.divide(50, timesResult);
-const powerResult = calculator.power(divideResult, 2);
-console.log(plusResult);
-console.log(minusResult);
-console.log(timesResult);
-console.log(divideResult);
-console.log(powerResult);
+if (isNaN(age)) {
+  alert("please write a number");
+}
